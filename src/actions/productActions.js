9d6ab155aef75774920783888e3979abbc4765e4
Yakov Gurevich <umanrh@gmail.com>
2021-08-23 16:26:01 +0300

    redux added

---
+++ src/actions/productActions.js
@@ -0,0 +1,23 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants";
+import axios from "axios";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get("http://localhost:5000/api/products");
+
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.responce && error.responce.data.message
+          ? error.responce.data.message
+          : error.message,
+    });
+  }
+};
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 // import products from "../products";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products, error, loading } = productList;
+
   useEffect(() => {
-    const getProducts = async () => {
-      const res = await axios.get(`http://localhost:5000/api/products`);
-      setProducts(res.data);
-      console.log(res.data);
-    };
-    getProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <>
       <h1>Latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
